7db895931a46f3ca942283509f2a7cce419db0a0
Ricardo Guimarães <68382012+ricardosguimaraes@users.noreply.github.com>
2025-08-02 11:33:57 -0300

    feat: adicionado botão do whatsapp

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import WhatsAppButton from '@/components/WhatsAppButton'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -49,7 +50,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <WhatsAppButton />
+      </body>
     </html>
   )
 } 
