c42c9be8f93a1a006e0ea7791bd426e299e57c41
Ricardo Guimarães <68382012+ricardosguimaraes@users.noreply.github.com>
2025-08-02 11:03:14 -0300

    feat: nova seção wholesale

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
+import Wholesale from '@/components/Wholesale'
 import Products from '@/components/Products'
 import Contact from '@/components/Contact'
 import Footer from '@/components/Footer'
@@ -10,6 +11,7 @@ export default function Home() {
       <Header />
       <Hero />
       <Products />
+      <Wholesale />
       <Contact />
       <Footer />
     </main>
